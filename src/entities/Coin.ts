ebc57a8e2d4398e95d4dcc5b28fcd698cd20357a
Ilmari Koskinen <ilmari.koskinen1@gmail.com>
2025-07-13 22:07:26 +0300

    remove obsolete tween

--- src/entities/Coin.ts
+++ src/entities/Coin.ts
@@ -1,24 +1,8 @@
 import { PickableItem } from "./PickableItem";
-import { gameConfig } from "../config/gameConfig";
 
 export class Coin extends PickableItem {
   constructor(scene: Phaser.Scene, x: number, y: number) {
     super(scene, x, y, "coin");
-
-    // Store the target display size
-    const targetSize = gameConfig.playerRadius * 2;
-
-    // Start with very small display size for bounce effect
-    this.setDisplaySize(targetSize * 0.01, targetSize * 0.01);
-
-    // Create bounce animation to full size
-    this.scene.tweens.add({
-      targets: this,
-      displayWidth: targetSize,
-      displayHeight: targetSize,
-      duration: 300,
-      ease: "Bounce.easeOut",
-    });
   }
 
   protected onPickupComplete(): void {
