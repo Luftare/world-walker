f9881c780f680e706f033496952823ca39497a71
Ilmari Koskinen <ilmari.koskinen1@gmail.com>
2025-07-13 18:52:44 +0300

    finally fix the compass lol

--- src/utils/Compass.ts
+++ src/utils/Compass.ts
@@ -44,57 +44,22 @@ export class UniversalCompass {
 
   private addListeners() {
     if (this.listenersAdded) return;
-    let didRemoveObsoleteListener = false;
 
-    const absoluteListener = (e: any) => {
-      // Remove the other listener
-      // Android Chrome, Desktop Chrome (no override)
-      if (!didRemoveObsoleteListener) {
-        window.removeEventListener("deviceorientation", defaultListener);
-        didRemoveObsoleteListener = true;
-        this.listenerType = "absolute";
-      }
-      this.onDeviceOrientation(e);
-    };
+    window.addEventListener(
+      "deviceorientation",
+      this.onDeviceOrientation.bind(this)
+    );
 
-    const defaultListener = (e: any) => {
-      // Remove the other listener
-      // iOS Safari, Desktop Chrome (override)
-      if (!didRemoveObsoleteListener) {
-        window.removeEventListener(
-          "deviceorientationabsolute",
-          absoluteListener
-        );
-        didRemoveObsoleteListener = true;
-        this.listenerType = "default";
-      }
-      debugLog("defaultListener");
-      this.onDeviceOrientation(e);
-    };
-
-    window.addEventListener("deviceorientation", defaultListener);
-
-    // const isAndroid = navigator.userAgent.toLowerCase().indexOf("android") > -1; //&& ua.indexOf("mobile");
-    // // We want only android devices to use the absolute listener
-
-    // if (isAndroid) {
-    //   window.addEventListener("deviceorientationabsolute", absoluteListener);
-    // } else {
-    //   // We want only iOS devices to use the default listener
-    //   window.addEventListener("deviceorientation", defaultListener);
-    // }
     this.listenersAdded = true;
   }
 
   private onDeviceOrientation(e: any): void {
     if (typeof e.webkitCompassHeading !== "undefined") {
       // iOS Safari
-      debugLog("webkitCompassHeading + " + this.listenerType);
       this.headingCallback?.(e.webkitCompassHeading);
     } else if (typeof e.alpha !== "undefined") {
       // Android Chrome
       const heading = 360 - e.alpha;
-      debugLog("alpha + " + this.listenerType);
       this.headingCallback?.(heading);
     }
     // If neither webkitCompassHeading nor alpha is available, ignore the event
